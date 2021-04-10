98cad2399c6bb1b49b9ce2183adb2a17864409e6
Tristan Martinuson <tmartinuson@gmail.com>
2021-04-10 15:55:19 -0500

    Added skeleton framework for tweet handles

--- venv/script/twitterFoodieBot.py
+++ venv/script/twitterFoodieBot.py
@@ -9,7 +9,7 @@ ACCESS_SECRET = 'BQbVLqYhOeYFvaA3OqbJMdxCUa60fxDCT3L9pl3aiEQUe'
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
-print("hey")
+print("Authcodes approved!")
 
 twitter_API = tweepy.API(auth)
 
@@ -18,5 +18,33 @@ def mention_followers():
     for follower in followers:
         twitter_API.update_status('Hello @' + follower.screen_name)
 
-twitter_API.update_status('Hello World')
+#twitter_API.update_status('Hello World')
+
+def get_memes(query, count):
+    tweets = []
+    fetch_tweets = twitter_API.search(query,count)
+
+    for tweet in fetch_tweets:
+        tweet
+
+
+def handle_mention_food():
+    #TODO Handle when someone tweets the bot with a meme and reply with a food recommendation
+    return
+
+def handle_mention_meme():
+    #TODO Handle when someone tweets the both with a food item and reply with a meme
+    return
+
+while True:
+    new_mentions = twitter_API.mentions_timeline(20)
+    if(len(new_mentions) > 0):
+        time.sleep(1)
+        # TODO if food
+        #   handle_mention_food
+        # TODO if meme
+        #   handle_mention_meme
+    time.sleep(120)
+
+
 
