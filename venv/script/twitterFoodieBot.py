adadd2351b7b8eef29958245a2afaf4ec653bd66
Tristan Martinuson <tmartinuson@gmail.com>
2021-04-10 19:20:08 -0500

    Added search for twitter account by query and reply to user mention with handle and link to twitter page

--- venv/script/twitterFoodieBot.py
+++ venv/script/twitterFoodieBot.py
@@ -1,5 +1,6 @@
 import tweepy
 import time
+import googlesearch
 
 CONSUMER_KEY = 'Z21yF4w1lrcTj4gL042ypnpYX'
 CONSUMER_SECRET = 'XmmVZPseQGMENqqvaLe0Teqjnov8UUryHSosxv77XV41QhyMH2'
@@ -33,16 +34,36 @@ def handle_mention(mention_object):
     except ImportError:
         print("No module named 'google' found")
 
+    query = query + "Twitter"
 
-    query = "Ice cream Vancouver"
+    restaurant_twitter_handle = ""
+    restaurant_url = ""
 
-    for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-        print(j)
+    for j in search(query, tld="co.in", num=1, stop=1, pause=2):
+        #Pick a random restaurant twitter
+        restaurant_twitter_handle = j + ""
+        restaurant_url = j
+        #print(j)
+
+    restaurant_twitter_handle = restaurant_twitter_handle[20:].split("?")[0]
+
+    #print(restaurant_twitter_handle)
+    print(restaurant_url)
+
+    user = twitter_API.get_user(screen_name = restaurant_twitter_handle)
+    best_tweet_of_user = twitter_API.user_timeline(user_id = user.id,count = 1)[0]
+    #print(user)
+    #print(best_tweet_of_user)
 
 
     #TODO Call twitter_API to reply to the mention handle with the URL of the restaurant
     ## FORMAT: @'mentioneehandle' https://www.somerestaurant.com
 
+    reply_username = mention_object.user.screen_name
+
+    status = "Enjoy! @" + reply_username + " " + restaurant_url
+
+    twitter_API.update_status(in_reply_to_status_id = mention_object.id, auto_populate_reply_metadata = True, status = status)
 
     #Possible TODO - reply with a screen shot of the google query
     return
@@ -58,6 +79,7 @@ while True:
             #Favorites tweet as not to reply
             #twitter_API.create_favorite(id = id)
             print(mention.text)
+            print(mention.id)
     time.sleep(120)
 
 
